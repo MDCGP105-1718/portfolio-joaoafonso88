02fdd74bb268d5e312353bc06fe88d5bd9f307c3
joao matias <matias_s_11@hotmail.com>
2017-10-05 11:41:29 +0100

    python file

--- xp5.py
+++ xp5.py
@@ -1,3 +1,4 @@
 for n in range (99,0,-1):
 	print(n)
-	print(f"{n} bottles of beer on the wall, {n} bottles of beer.Take one down, pass it around, {n} bottles of beer on the wall…")
+	print(if"{n} bottles of beer on the wall, {n} bottles of beer.Take one down, pass it around, {n} bottles of beer on the wall...")
+	print "no more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, {n} bottles of beer on the wall..."
