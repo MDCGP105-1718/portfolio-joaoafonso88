abb43abb5044c2e385f4f9f525672fd8b26740f7
joaoafonso88 <32357035+joaoafonso88@users.noreply.github.com>
2018-01-11 21:55:22 +0000

    Add files via upload

---
+++ contact_us.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- The width=device-width part sets the width of the page to follow the screen-width of the device (which will vary depending on the device).
+    The initial-scale=1 part sets the initial zoom level when the page is first loaded by the browser. -->
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+     <title>
+
+        British Telemark
+
+    </title>
+    <link href="css/main.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- this is a link for font awesome icons-->
+
+</head>
+
+
+<!-- body starts here -->
+<body class="">
+
+<div id="wrapper" class="container"> <!-- container is a class provided by bootstrap.. -->
+   
+        <div class="navbar-inner main">
+            <a href="" class="blue_a" style="text-decoration: none;"><img src="images/logo.jpg"> &nbsp;&nbsp;&nbsp;&nbsp; <span style="font-size:18px;">British Telemark</span> </a> <!-- &nbsp; is used for blank space -->
+<input type="search" class="top_search txt_search" placeholder="Search.." >
+        </div>
+
+
+
+            
+<!--  code for menus starts here -->
+
+<div class="menu">
+
+  <ul class="menu-ul">
+<input type="search" class="txt_search" placeholder="Search.." >
+    <a href="sponser.php" class="blue_a"><li>Sponser</li></a>
+      <a href="news.php" class="blue_a"><li>News</li></a>
+
+    <a href="contact_us.php" class="blue_a"><li>Contact us</li></a>
+    <a href="index.php" class="blue_a"><li>Home</li></a>
+  </ul>
+</div>
+
+
+
+
+<!-- code for social media icons and links start here -->
+<div class="share"> <!-- share class has display fixed. so, it will stock on screen even when scrolling -->
+<a href="https://www.facebook.com" class="fa fa-facebook fa1 " target="_blank"></a>
+<!--  fa fa-facebook is a class provided by font awesome used for displaying facebook icon -->
+
+<a href="https://www.twitter.com" class="fa fa-twitter fa1" target="_blank"></a>
+<!--  fa fa-twitter is a class used for displaying twitter icon -->
+
+<a href="https://www.gmail.com" class="fa fa-google fa1" target="_blank"></a>
+<!--  fa fa-google is a class used for displaying google icon -->
+
+<a href="https://www.linkedin.com" class="fa fa-linkedin fa1" target="_blank"></a>
+<!--  fa fa-linkedin is a class used for displaying linkedin icon -->
+
+<a href="https://www.youtube.com" class="fa fa-youtube fa1" target="_blank"></a>
+<!--  fa fa-youtube is a class used for displaying youtube icon -->
+
+<a href="https://www.instagram.com" class="fa fa-instagram fa1" target="_blank"></a>
+<!--  fa fa-instagram is a class used for displaying instagram icon -->
+</div>
+
+
+ 
+<div class="row">   <!-- creating a row --> 
+
+
+	<div class="col-md-6 pos3" > <!-- each row contains 12 cols and col-md-6 is a class that groups 6 columns together -->
+
+  <!--  code for providing contact information of British Telemark starts here -->
+		<h1>Contact</h1>
+		
+			<p><strong>General enquiries:</strong><br>Team Captain – Louis Hatchwell<br><a href="#" class="blue_a">LouisJ4@hotmail.com</a></p> 
+
+            <p><strong>Partnership & sponsorship enquiries:</strong><br>
+            Sponsorship coordinator – Alison Morrissey<br>
+              <a href="#" class="blue_a">ali.morrissey@btinternet.com</a>  /  +44(0)7775847934</p>
+
+            <p><strong>Media enquiries:</strong><br>
+             Marketing Communications coordinator – Jack Harvard Taylor<br>
+              <a href="#" class="blue_a">jackharvardtaylor@gbtelemark.co.uk</a>  /  +44(0)7799300140</p><br>
+              <div >
+
+              <img src="images/contact.jpg">
+
+          </div>
+		
+	</div>
+
+
+  <!-- code for receiving customer's feedback starts here -->
+
+	<div class="col-md-5 pos4">
+		
+		<form action="" method="post">
+			
+         <input type="text" name="name" id="name" Placeholder="NAME..." required>
+         <input type="text" name="contact" id="contact" placeholder="CONTACT NO..." required>
+         <input type="email" name="mail" id="mail" Placeholder="EMAIL ID..." required>
+         <textarea id="msg" name="msg" placeholder="MESSAGE..." required></textarea>
+         <button><i class="fa fa-envelope"></i>&nbsp;&nbsp;Send Message</button> <!-- fa fa-envelop is a class used for displaying envelop icon -->
+		</form>
+		
+	</div>
+
+  
+</div>
+
+
+
+ <!-- code for footer starts here -->
+
+  <div class="col-md-12 back">
+   
+        <p style="text-align:center;"><strong>&copy;British Telemark Ski Team 2017</strong></p> <!-- &copy; is a code for copyright icon -->
+        <div class="shr_div">
+
+<!--    code for social medias links start here -->
+        <center>
+          <a href="#" class="shr"><button class="btn_share_twt btn_share"><i class="fa fa-twitter"></i> Tweet</button></a> 
+               <!--  fa fa-twitter is a class used for displaying twitter icon -->
+          <a href="#" class="shr"><button class="btn_share_fb btn_share"><i class="fa fa-thumbs-up"></i> Like 71</button></a>
+               <!--  fa fa-thumbs-up is a class used for displaying thumbs-up icon -->
+          <a href="#" class="shr"><button class="btn_share_g btn_share"><i class="fa fa-google"></i>+</button></a>
+                <!-- fa fa-google is a class used for displaying google icon -->
+          <a href="#" class="shr"><button class="btn_share_li btn_share"><i class="fa fa-linkedin" ></i> Share</button></a>
+                <!-- fa fa-linkedin is a class used for displaying linkedin icon -->
+          <a href="#" class="shr"><button class="btn_share_fb btn_share"><i class="fa fa-facebook"></i> Share 71</button></a>
+                <!-- fa fa-facebook is a class used for displaying facebook icon -->
+        </center>
+        </div>  
+        <br><hr>  
+
+  </div>
+   <div class="c_foot">Sponsored by Krystal Hosting</div>
+
+</div>
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- The width=device-width part sets the width of the page to follow the screen-width of the device (which will vary depending on the device).
+    The initial-scale=1 part sets the initial zoom level when the page is first loaded by the browser. -->
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+     <title>
+
+        British Telemark
+
+    </title>
+    
+    <link href="css/main.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- this is a link for font awesome icons-->
+
+</head>
+
+
+<!-- body starts here -->
+<body class="">
+
+<div id="wrapper" class="container"> 
+   
+        <div class="navbar-inner main">
+            <a href="" class="blue_a" style="text-decoration: none"><img src="images/logo.jpg"> &nbsp;&nbsp;&nbsp;&nbsp; <span style="font-size:18px;">British Telemark</span> </a> <!-- &nbsp; is used for blank space -->
+<input type="search" class="top_search txt_search" placeholder="Search.." >
+        </div>
+
+
+
+            
+<!--  code for menus starts here -->
+
+<div class="menu">
+
+  <ul class="menu-ul">
+<input type="search" class="txt_search" placeholder="Search.." >
+    <a href="sponser.php" class="blue_a"><li>Sponser</li></a>
+      <a href="news.php" class="blue_a"><li>News</li></a>
+
+    <a href="contact_us.php" class="blue_a"><li>Contact us</li></a>
+    <a href="index.php" class="blue_a"><li>Home</li></a>
+  </ul>
+</div>
+
+
+
+
+<!-- code for social media icons and links start here -->
+<div class="share"> <!-- share class has display fixed. so, it will stock on screen even when scrolling -->
+<a href="https://www.facebook.com" class="fa fa-facebook fa1 " target="_blank"></a>
+<!--  fa fa-facebook is a class provided by font awesome used for displaying facebook icon -->
+
+<a href="https://www.twitter.com" class="fa fa-twitter fa1" target="_blank"></a>
+<!--  fa fa-twitter is a class used for displaying twitter icon -->
+
+<a href="https://www.gmail.com" class="fa fa-google fa1" target="_blank"></a>
+<!--  fa fa-google is a class used for displaying google icon -->
+
+<a href="https://www.linkedin.com" class="fa fa-linkedin fa1" target="_blank"></a>
+<!--  fa fa-linkedin is a class used for displaying linkedin icon -->
+
+<a href="https://www.youtube.com" class="fa fa-youtube fa1" target="_blank"></a>
+<!--  fa fa-youtube is a class used for displaying youtube icon -->
+
+<a href="https://www.instagram.com" class="fa fa-instagram fa1" target="_blank"></a>
+<!--  fa fa-instagram is a class used for displaying instagram icon -->
+</div>
+
+
+
+
+<!-- code for articles and main body starts here -->
+<div class="row"> <!-- creating a row -->  
+
+
+	<div class="col-md-12"><img src="images/home.jpg" class="r_image"></div>  <!-- each row has 12 column and col-md-12 is a class that groups 12 columns together -->
+
+
+<div class="col-md-7 pos2"> <!-- col-md-7 is a class that groups 7 columns together  -->
+
+    <!--  Heading -->
+    <br><h1 style="font-variant: inherit;">WELCOME</h1><br><br>
+
+    <!-- Article starts here -->
+    <h4 style="font-variant: inherit;"><b>Welcome to the home of the British Telemark Ski Team</b></h4><br>
+    <p>Governed under <a href="#" class="blue_a">British Ski and Snowboard</a> (BSS), the National Governing Body for Skiing and Snowboarding in the United Kingdom, the British Telemark Team is a recognised power on the world stage and within the <a href="#" class="blue_a">International Ski Federation</a> (FIS).</p><br><p>Founded in 2006, the team has gone from strength-to-strength representing Great Britain at the highest level on the FIS Telemark World Cup circuit.</p><br>
+
+
+   <!--  Embedding youtube video using <iframe> tag -->
+    <div><p><iframe width="100%" height=300px src="https://www.youtube.com/embed/QY1aVW9DYT8?list=UU-1byFxGExShYzxRl-lPPmw" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen style="border:1px solid #60B0E6;" class="thumble" ></iframe></p></div> 
+
+
+    
+     <br><p>With the addition of exhilarating new disciplines, such as the Telemark Parallel Sprint (TPS), the sport is constantly evolving as is the level of competition and the levels of participation.</p><p class="bo">On the website you can see current news, athlete profiles, latest results and information about how you can get involved in our unique sport!</p>
+      </div>
+
+     <!-- Article ends here -->
+    
+
+
+
+    <!-- code for aside starts here -->
+    <div class="col-md-4 pos" style="float:left;" >
+    
+    <div class="gallery thumble">
+        <a href="#" class="blue_a"><h5>BRITISH TELEMARK SKI TEAM</h5></a>
+        <img src="images/member.jpg" width="100%">
+        <div class="desc">
+         Be the first of your friend to like<br>
+         <a href="#"><img class="m_img" src="images/m1.jpg" ></a><a href="#"><img src="images/m2.jpg" class="m_img"></a><a href="#"><img src="images/m3.jpg" class="m_img"></a><a href="#"><img src="images/m4.jpg" class="m_img"></a><a href="#"><img src="images/m5.jpg" class="m_img"></a>
+         <img>
+        </div>
+        <div class="abs"><img src="images/logo.jpg" style="width:70px;height:70px;"></div>
+        <div class="abs_2"> <h3>GB Telemark Ski Team</h3> <h5 style="float:left;"><strong>1,335 likes</strong></h5></div>
+        <div class="abs_3"><a href="#"><button class="lp"><i class="fa fa-facebook"></i>&nbsp; Like page</button></a></div>
+    <!-- abs, abs_2 and abs_3 are classes with position:absolute and they are positioned relative to the nearest positioned ansestor -->
+
+    </div>
+    <br>
+
+
+   <!--  Code for Meta key starts here -->
+    <div class="list_div"><span class="list_span" ><b>META</b></span><br>
+    <ul class="link">
+        <a href="#" class="blue_a"><li><b>Log in</b></li></a>
+        <a href="#" class="blue_a"><li><b>Entries RSS</b></li></a>
+        <a href="#" class="blue_a"><li><b>Comments RSS</b></li></a>
+        <a href="#" class="blue_a"><li><b>Wordpress.org</b></li></a>
+
+    </ul>
+</div>
+
+
+</div>
+    </div>
+
+    
+    
+
+
+<!-- code for footer starts here -->
+
+  <div class="col-md-12 back">
+   
+        <p style="text-align:center;"><strong>&copy;British Telemark Ski Team 2017</strong></p> <!-- &copy; is a code for copyright icon -->
+        <div class="shr_div">
+
+<!--    code for social medias links start here -->
+        <center>
+          <a href="#" class="shr"><button class="btn_share_twt btn_share"><i class="fa fa-twitter"></i> Tweet</button></a> 
+               <!--  fa fa-twitter is a class used for displaying twitter icon -->
+          <a href="#" class="shr"><button class="btn_share_fb btn_share"><i class="fa fa-thumbs-up"></i> Like 71</button></a>
+               <!--  fa fa-thumbs-up is a class used for displaying thumbs-up icon -->
+          <a href="#" class="shr"><button class="btn_share_g btn_share"><i class="fa fa-google"></i>+</button></a>
+                <!-- fa fa-google is a class used for displaying google icon -->
+          <a href="#" class="shr"><button class="btn_share_li btn_share"><i class="fa fa-linkedin" ></i> Share</button></a>
+                <!-- fa fa-linkedin is a class used for displaying linkedin icon -->
+          <a href="#" class="shr"><button class="btn_share_fb btn_share"><i class="fa fa-facebook"></i> Share 71</button></a>
+                <!-- fa fa-facebook is a class used for displaying facebook icon -->
+        </center>
+        </div>  
+        <br><hr>  
+
+  </div>
+
+  
+   <div class="c_foot">Sponsored by Krystal Hosting</div>
+
+</div>
+</body>
+</html>
